6f9ff10270d0986464985ee0e59abbac6289d114
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-08-22 10:42:36 +0200

    1

--- 1_regresja_liniowa.py
+++ 1_regresja_liniowa.py
@@ -3,12 +3,30 @@ import matplotlib.pyplot as plt     # biblioteka do wykresów
 import seaborn as sns               # biblioteka do wykresów
 from sklearn.linear_model import LinearRegression    # regresja liniowa
 
+# czytanie danych
 df = pd.read_csv('weight-height.csv', delimiter=';')
 print(df)
+
+# obrobka danych
 df.Height *= 2.54     # df.Height = df.Height * 2.54
 df.Weight /= 2.2
 print('obrobione dane')
 print(df.head(10))    # wyswietl 10 pierwszych wierszy
+
+# wykres przy pomocy plt
+plt.hist(df.Weight)    # przygotowanie wykresu
+plt.show()             # wyswietlenie wykresu
+
+# wykres przy pomocy sns
+sns.histplot(df.Weight)    # przygotowanie wykresu
+plt.show()             # wyswietlenie wykresu
+
+# problem - mezczyzni i kobiety razem
+# wykres przy pomocy sns
+sns.histplot(df.query('Gender=="Female"').Weight)    # przygotowanie wykresu, same kobiety
+# plt.show()
+sns.histplot(df.query('Gender=="Male"').Weight)    # przygotowanie wykresu, sami panowie
+plt.show()
 
 
 
