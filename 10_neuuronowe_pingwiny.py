71ff923783241ce76e2f10a47b1472f95cc12bdb
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-08-23 13:26:38 +0200

    1

--- 10_neuuronowe_pingwiny.py
+++ 10_neuuronowe_pingwiny.py
@@ -19,15 +19,15 @@ X_train, X_test, y_train, y_test = train_test_split(penguins_features, penguins_
 
 inputs = keras.Input(shape=[X_train.shape[1]])
 hidden_layer1 = keras.layers.Dense(20, activation='relu')(inputs)
-hidden_layer2 = keras.layers.Dense(20, activation='linear')(hidden_layer1)
-hidden_layer3 = keras.layers.Dense(20, activation='linear')(hidden_layer2)
+hidden_layer2 = keras.layers.Dense(20, activation='relu')(hidden_layer1)
+hidden_layer3 = keras.layers.Dense(20, activation='relu')(hidden_layer2)
 output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer3)
 
 model = keras.Model(inputs=inputs, outputs=output_layer)
 # print(model.summary())
 
 model.compile(optimizer='adam', loss=keras.losses.CategoricalCrossentropy())
-result = model.fit(X_train, y_train, epochs=1000)
+result = model.fit(X_train, y_train, epochs=2000)
 # print(result.history)
 sns.lineplot(x=result.epoch, y=result.history['loss'])
 plt.show()
