8729554c11ecca1f008b598159268daec3dab9b3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-08-23 10:04:15 +0200

    1

---
+++ 7_siatka_parametrow.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv('heart.csv', comment='#')
+print(df.head().to_string())     # wyświetl 5 pierwszych wierszy, wszystkie kolumny
+print(df.describe().T.to_string())
+# klasy zbalansowane
+
+X = df.iloc[:, :-1]      # wszystkie kolumny bez ostatniej
+y = df.target            # ostatnia kolumna
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier(criterion='gini', max_depth=6, min_samples_split=2)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+print(pd.DataFrame(model.feature_importances_, X.columns))
+
+print('Siatka parametrów')
+model = DecisionTreeClassifier()
+# model = RandomForestClassifier()
+params = {
+    'max_depth': range(5, 20, 4),
+    'min_samples_split': range(2, 7),
+    'criterion': ['gini', 'entropy', 'log_loss'],
+#    'max_features': range(3, X_train.shape[1]+1, 2)
+}
+
+grid = GridSearchCV(model, params, cv=5, verbose=2)     # cv - cross validation
+grid.fit(X_train, y_train)
+print(f'parametry: {grid.best_params_}')   # najlepsze parametry
+print(f'wynik: {grid.best_score_}')        # najlepszy wynik
+allscores = grid.cv_results_['mean_test_score']
+print(allscores)
+
+# zapis i oczyt modelu
+import joblib
+joblib.dump(grid, 'Kamil_model1.model')     # zapis modelu
+new_model = joblib.load('Kamil_model1.model')
